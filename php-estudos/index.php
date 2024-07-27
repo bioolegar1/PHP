5866dd964f9d07b73f8acc4ca7718b7dc17bcb4f
bio <bioolegari@gmail.com>
2024-07-27 11:51:40 -0300

    estruturas if

--- php-estudos/index.php
+++ php-estudos/index.php
@@ -6,17 +6,10 @@ include_once 'sistema/helpers.php';
 
 $texto = "Texto para Resumir";
 
-$strig = 'Texto';
-$int = 10;
-$float = 3.1;
-$bool = true; //ou false
-$null = null; //diferente de vazio
 
-
-//var_dump($int, $float, $bool, $null, $strig);
 
 
 echo saudacao();
 echo '<hr>';
-echo resumirTexto($texto, 100, 'continue');
+//echo resumirTexto($texto, 100, 'continue');
 ?>
--- php-estudos/sistema/helpers.php
+++ php-estudos/sistema/helpers.php
@@ -1,7 +1,21 @@
     <?php
 function saudacao(): string
 {
-    return "Bem-vindo ao sistema!";
+    $hora = 19;
+    $saudacao = '';
+
+    if($hora >= 0 && $hora <= 5){
+        $saudacao = 'Boa Madrugada';
+    }
+    if($hora >= 6 && $hora <= 17){
+        $saudacao = 'Bom dia';
+    }
+
+    if($hora >= 18 && $hora <= 24){
+        $saudacao = 'Boa Noite';
+    }
+
+    return "$saudacao";
 }
 
 function resumirTexto(string $texto, int $limite, string $continue = '...'): string
